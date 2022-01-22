75850fb05a144548b0bb668ebc5f1538b23649c1
Piotr Pasich <piotr.pasich@gmail.com>
2022-01-22 12:00:03 +0100

    tor1

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -18,15 +18,17 @@
 
 #define FORWARD 0
 #define BACKWARD 1
-#define MAX_SPEED 250
-double multiplier = 0.4;
-double GO_BACK_MULTIPLIER = 0.7;
+#define MAX_SPEED 200
+double multiplier = 0.01;
+double maxSpeedMultiplier = 0.4;
+double GO_BACK_MULTIPLIER = 0.0;
 
+int tor = 1;
 
 // Global variables and defines
 
 // object initialization
-DCMDriverL298 dcMotorDriverL298(DCMOTORDRIVERL298_PIN_ENA,DCMOTORDRIVERL298_PIN_INT1,DCMOTORDRIVERL298_PIN_INT2,DCMOTORDRIVERL298_PIN_ENB,DCMOTORDRIVERL298_PIN_INT3,DCMOTORDRIVERL298_PIN_INT4);
+//DCMDriverL298 dcMotorDriverL298(DCMOTORDRIVERL298_PIN_ENA,DCMOTORDRIVERL298_PIN_INT1,DCMOTORDRIVERL298_PIN_INT2,DCMOTORDRIVERL298_PIN_ENB,DCMOTORDRIVERL298_PIN_INT3,DCMOTORDRIVERL298_PIN_INT4);
 
 
 
@@ -47,6 +49,11 @@ void setup()
     pinMode(IRLINEFOLLOW_1_PIN_OUT, INPUT);
     pinMode(IRLINEFOLLOW_2_PIN_OUT, INPUT);
     pinMode(IRLINEFOLLOW_3_PIN_OUT, INPUT);
+
+    pinMode(DCMOTORDRIVERL298_PIN_INT1, OUTPUT);
+    pinMode(DCMOTORDRIVERL298_PIN_INT2, OUTPUT);
+    pinMode(DCMOTORDRIVERL298_PIN_INT3, OUTPUT);
+    pinMode(DCMOTORDRIVERL298_PIN_INT4, OUTPUT);  
 }
 
 bool leftLineCrossed = false;
@@ -57,64 +64,45 @@ void loop()
     bool rightSensor = digitalRead(IRLINEFOLLOW_1_PIN_OUT);
     bool centerSensor = digitalRead(IRLINEFOLLOW_3_PIN_OUT);
 
-    double MULTIPLIER = multiplier;
 
-    int leftMotorSpeed = MAX_SPEED * (leftSensor && !centerSensor ? MULTIPLIER : 1);
-    int rightMotorSpeed = MAX_SPEED * (rightSensor && !centerSensor ? MULTIPLIER : 1);
+
+    // prosty algorytm
+    int rightMotorSpeed = MAX_SPEED;
+    int leftMotorSpeed = MAX_SPEED;
 
     if (leftSensor) {
-      leftLineCrossed = true;
+      rightMotorSpeed = 0;
+      leftMotorSpeed = leftMotorSpeed * maxSpeedMultiplier;
     }
     if (rightSensor) {
-      rightLineCrossed = true;
+      leftMotorSpeed = 0;
+      rightMotorSpeed = rightMotorSpeed * maxSpeedMultiplier;
     }
-    if (centerSensor && (rightLineCrossed || leftLineCrossed)) {
-      if (leftLineCrossed) {
-        rightMotorSpeed = MAX_SPEED * GO_BACK_MULTIPLIER;
-        leftMotorSpeed = MAX_SPEED;
-      }
-      if (rightLineCrossed) {
-        leftMotorSpeed = MAX_SPEED * (GO_BACK_MULTIPLIER) ;
-        rightMotorSpeed = MAX_SPEED;
-      }
-      dcMotorDriverL298.setMotorA(rightMotorSpeed, FORWARD);
-      dcMotorDriverL298.setMotorB(leftMotorSpeed, FORWARD);
-      Serial.println("BACK");
-      delay(200);
-      leftLineCrossed = false;
-      rightLineCrossed = false;
-    }
-    
-    
-    if (!centerSensor && !leftSensor && !rightSensor) {
-      if (leftLineCrossed) {
-        leftMotorSpeed = MAX_SPEED * GO_BACK_MULTIPLIER;
-        rightMotorSpeed = MAX_SPEED;
-      } 
-      if (rightLineCrossed) {
-        rightMotorSpeed = MAX_SPEED * GO_BACK_MULTIPLIER;
-        leftMotorSpeed = MAX_SPEED;
-      } 
-    } 
 
-    
+       
     Serial.println("|:");
     Serial.print(leftSensor);
-    Serial.print("|");
+    Serial.print("|ls");
     Serial.print(leftMotorSpeed);
     Serial.print("|c:");
     Serial.print(centerSensor );
     Serial.print("|");
     Serial.print(rightSensor);
-    Serial.print("|");
+    Serial.print("|rs");
     Serial.print(rightMotorSpeed);
     Serial.print("|");
-    Serial.print(MULTIPLIER);
+    Serial.print(maxSpeedMultiplier);
     Serial.print("|");
     
-    dcMotorDriverL298.setMotorA(rightMotorSpeed, FORWARD);
-    dcMotorDriverL298.setMotorB(leftMotorSpeed, FORWARD);
-    delay(5);
+//    dcMotorDriverL298.setMotorA(rightMotorSpeed, FORWARD);
+  
+//    dcMotorDriverL298.setMotorB(leftMotorSpeed, FORWARD);
+    analogWrite(DCMOTORDRIVERL298_PIN_INT1, rightMotorSpeed);
+    digitalWrite(DCMOTORDRIVERL298_PIN_INT2, LOW);
+    
+    analogWrite(DCMOTORDRIVERL298_PIN_INT3, leftMotorSpeed);
+    digitalWrite(DCMOTORDRIVERL298_PIN_INT4, LOW);
+    //delay(1);
     //Stop both motors
     //dcMotorDriverL298.stopMotors();
     //delay(200);    
